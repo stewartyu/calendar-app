9c0f27f271165881624393510a7d65bb8376d363
Stewart Yu <stewartyu@gmail.com>
2016-02-29 20:05:23 -0800

    Podfile and Info.plist setup

---
+++ Pods/Button/Button.framework/Versions/A/Headers/Button_Public.h
@@ -0,0 +1,26 @@
+
+// Session
+#import "BTNSession.h"
+
+// UI Components
+#import "BTNDropinButton.h"
+#import "BTNDropinButtonCell.h"
+
+// Context
+#import "BTNContext.h"
+
+// App Action
+#import "BTNAppAction.h"
+
+// Orders
+#import "BTNLineItem.h"
+
+
+// System
+@import SystemConfiguration;
+@import MobileCoreServices;
+@import Security;
+@import StoreKit;
+@import AdSupport;
+@import Accelerate;
+@import AVFoundation.AVUtilities;
---
+++ Pods/Target Support Files/Pods-Calendar/Pods-Calendar-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CalendarVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CalendarVersionString[];
+
